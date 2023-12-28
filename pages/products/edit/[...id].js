48ab59fe7d5c9b52dbfb1611b90378332da4f05d
harshit <harshit302001n@gmail.com>
2023-12-29 00:57:33 +0530

    first commit

---
+++ pages/products/edit/[...id].js
@@ -0,0 +1,28 @@
+import ProductForm from "@/components/ProductForm";
+import Layout from "@/components/layout";
+import axios from "axios";
+import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
+
+export default function EditProductPage() {
+  const router = useRouter();
+  const { id } = router.query;
+  const [productInfo, setProductInfo] = useState(null);
+
+  useEffect(() => {
+    if (!id) {
+      return;
+    }
+
+    axios.get("/api/products?id=" + id).then((response) => {
+      setProductInfo(response.data);
+    });
+  }, [id]);
+
+  return (
+    <Layout>
+      <h1>Edit Product</h1>
+      {productInfo && <ProductForm {...productInfo} />}
+    </Layout>
+  );
+}
